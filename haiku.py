77a3c5e99861f98e1c6f67b6ad17859091806279
jasminchang <53197556+jasminchang@users.noreply.github.com>
2019-08-16 10:04:06 -0700

    Create haiku.py

---
+++ haiku.py
@@ -0,0 +1,17 @@
+from random import*
+
+fivesyl=["electricity", "pediatrician", "university", "similarity"]
+sevensyl=[" intercolonization", " sentimentalization", " superficiality", " iriversibility"]
+fivesyl2=[ " vocabulary", " intimidating", " mathmatical", " appreciation"]
+
+aRandomIndex=randint(0, len(fivesyl)-1)
+bRandomIndex=randint(0, len(sevensyl)-1)
+cRandomIndex=randint(0, len(fivesyl2)-1)
+
+print(fivesyl[aRandomIndex] + sevensyl[bRandomIndex] + fivesyl2[cRandomIndex])
+
+
+
+
+
+ 
